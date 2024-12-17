43bb37e63ea2c5d732dd1af1d7148c5847570883
Dhruv Patel <68446541+Rigatoni17@users.noreply.github.com>
2024-12-16 19:21:40 -0500

    Update pagerank_app.py

--- pagerank_app.py
+++ pagerank_app.py
@@ -6,14 +6,6 @@ import pandas as pd
 import json
 import tempfile
 
-hide_streamlit_style = """
-    <style>
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-    </style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 # Import the functions from Topic3.ipynb
 import functions
 
